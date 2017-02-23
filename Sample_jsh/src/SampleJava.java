fe8e6211e222c184743864cdc5d72b4a7285d43c
Jagadeesh <Narni@192.168.0.6>
2017-02-23 11:43:59 +0530

    Added New message

---
+++ Sample_jsh/src/SampleJava.java
@@ -0,0 +1,31 @@
+import ChildJava.inherit;
+
+/**
+ * 
+ */
+
+/**
+ * @author Narni
+ *
+ */
+public class SampleJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi welcome to java");
+		displayData();
+		displayOfficeName();
+		
+	}
+	public static void displayData(){
+		System.out.println("Welcome to Akshar");
+	}
+	public static void displayOfficeName(){
+		System.out.println("Welcome To Akshar School Solutions");
+	}
+
+
+}
